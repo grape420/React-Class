bcc5841c45e78b4c91853284de90dd3f779cf0bc
grape420 <grape420@naver.com>
2023-04-08 15:58:40 +0900

    TypeScript 타입 추론 예제 작성 완료

---
+++ react-weekend/lab02/ex01.ts
@@ -0,0 +1,46 @@
+// TypeScript는 변수를 선언할 때 타입 주석(type annotation)을 사용한다.
+let x = 1;
+// x = "hello";
+// -> TS에서는 타입이 다른 값을 재할당할 수 없음.
+console.log(`x : ${x}`);
+console.log(typeof x);
+
+// TS 파일 실행
+// (1) tsc xyz.ts -> TS를 JS로 변환
+// (2) node xyz.ts 0> JS를 node 환경에서 실행
+
+// TS 파일 실행 2
+// ts-node xyz.ts -> 변환과 실행을 동시에 함
+
+// TS는 타입 추론 기능이 있음. 할당하는 값을 보고 변수의 타입을 결정
+let y = "hello";
+// y = 123  // -> 다른 타입의 값은 저장할 수 없음.
+console.log(`y : ${y}`);
+
+// TS에서는 함수에서의 타입을 정해줘야 함. 리턴 타입은 타입 안 정해줘도 됨
+const plusOne = (z: number) => z + 1;
+console.log(plusOne(2));
+
+const user = {
+  id: 1,
+  name: "김종현",
+  obj: {},
+};
+
+// 객체가 갖는 프로퍼티들의 타입을 선언 - 객체의 모양 선언
+interface User {
+  id: number;
+  name: string;
+  obj: object;
+}
+
+function showUser(user: User) {
+  console.log(`id : ${user.id}, name : ${user.name}, obj : ${user.obj}`);
+}
+showUser(user);
+
+// Union(합집합) 기능을 사용해서 간단한 타입들을 조합한 새로운 타입을 선언
+type LockState = "locked" | "unlocked";
+let isLocked: LockState = "locked";
+
+console.log(isLocked);
