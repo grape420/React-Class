d4457f4b4c90e8124ebc98885a7dcc52029437b5
grape420 <grape420@naver.com>
2023-04-08 14:12:08 +0900

    forEach, filter, reduce 메소드 예제 작성 완료

--- react-weekend/lab01/ex05.js
+++ react-weekend/lab01/ex05.js
@@ -46,3 +46,79 @@ console.log(mapped);
 // 배열 front에서 5글자 이상인 문자열들을 대문자로 변환 후 출력
 front.filter((e) => e.length >= 5).map((e) => console.log(e.toUpperCase()));
 // .forEach((e) => console.log(e));
+
+/*
+  arrow function
+  (param, ...) => ${ statement; ...}
+  (param, ...) => statement : 함수 body에 실행문이 하나만 있는 경우
+  (param, ...) => 리턴값 : 함수 body에 실행문이 하나만 있고 실행문이 리턴문인 경우
+ */
+
+/*
+  reduce(callback, initialValue) : 배열의 모든 원소를 순회해서 값 1개를 리턴
+  callback 함수의 argument들
+  - previousValue : callback 함수에서 이전에 리턴된 값
+  - currentValue : 반복하고 있는 현재 배열의 값
+  - currentIndex : 반복하고 있는 현재 배열의 인덱스
+  - array : 반복하고 있는 배열 자체
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sum = numbers.reduce((total, current) => (total += current), 100);
+const factorial = numbers.reduce((total, current) => (total *= current), 1);
+// console.log(`sum : ${sum}`);
+// console.log(`factorial : ${factorial}`);
+// console.log(`numbers : ${numbers}`);
+
+// 쿼리스트링을 분석해서 객체로 변환하는 함수
+const queryString = 'apple=10&banana=20&order=Y';
+// -> {apple : 10, banana : 20, order : Y}
+function parse(qs) {
+  let result = {};
+  const chunks = qs.split('&'); // -> ['apple=10', 'banana=20', 'order=Y']
+  for (let i of chunks) {
+    const [key, value] = i.split('=');
+    // const part = i.split('='); // -> ['apple', '10']
+    // const key = part[0];
+    // const value = part[1];
+    result[key] = value;
+  }
+  return result;
+}
+function parse2(qs) {
+  let result = {};
+  const chunks = qs.split('&');
+  chunks.forEach((x) => {
+    const [key, value] = x.split('=');
+    result[key] = value;
+  });
+  return result;
+}
+
+function parse3(qs) {
+  return qs.split('&').map((x) => {
+    // x='apple=10'
+    const [key, value] = x.split('='); // key = apple, value = 10
+
+    // return { key : key, value : value }; // key와 value가 값이 동일하다면 생략해서 사용 가능
+    return { key, value };
+  });
+}
+
+function parse4(qs) {
+  return qs
+    .split('&')
+    .map((x) => {
+      const [key, value] = x.split('='); // [key, value] = [apple, 10]
+      return { key, value }; // [{key : apple, value : 10}, {key : banana, value : 20}, {key : order, value : Y}]
+    })
+    .reduce((prev, curr) => {
+      prev[curr.key] = curr.value; // {apple : 10, banana : 20, order : Y}
+      return prev;
+    }, {});
+}
+
+// console.log(parse(queryString));
+// console.log(parse2(queryString));
+// console.log(parse3(queryString));
+console.log(parse4(queryString));
